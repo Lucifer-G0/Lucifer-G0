3ac9ba8f5cfdf943861dd46c4230e6e5a612375d
jacklee <jacklee921177@gmail.com>
2022-04-10 12:36:04 +0800

    test_cluster_blue

---
+++ test_cluster_blue/ObjectWindow.h
@@ -0,0 +1,27 @@
+#ifndef _ObjectWindow_H_
+#define _ObjectWindow_H_
+
+#include <opencv2/opencv.hpp>
+#include <pcl/point_types.h>
+#include <vtkType.h>
+// opencv 画框使用的物体窗
+class ObjectWindow
+{
+public:
+    int topleft_x;
+    int topleft_y;
+    int width;
+    int height;
+    ObjectWindow():topleft_x(-1),topleft_y(-1),width(-1),height(-1){}
+    ObjectWindow(int x, int y, int w, int h):topleft_x(x),topleft_y(y),width(w),height(h){}
+    void output();
+    void update();
+    cv::Mat draw(cv::Mat image);
+    void add_point(pcl::PointXYZ point);
+private:
+    // set min to MAX so that it will changed immediately when compare
+    float min_x=VTK_FLOAT_MAX, min_y=VTK_FLOAT_MAX, max_x=VTK_FLOAT_MIN, max_y=VTK_FLOAT_MIN;
+    float constant = 570.3;
+};
+
+#endif
---
+++ test_cluster_blue/test_cluster_blue.cpp
@@ -0,0 +1,136 @@
+#include <pcl/io/pcd_io.h>
+#include <pcl/console/parse.h>
+#include <pcl/segmentation/extract_clusters.h> // Euclidean Cluster Extract
+#include <string>
+#include "ObjectWindow.h"
+
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
+
+int main(int argc, char **argv)
+{
+    PointCloudT::Ptr cloud(new PointCloudT);
+    pcl::PCDWriter writer;
+    std::string image_path = "00000-color.png";
+    std::string pcd_path = "blue.pcd";
+
+    // load cloud from pcd file
+    pcl::console::print_highlight("Loading point cloud...\n");
+    if (pcl::io::loadPCDFile<PointT>(pcd_path, *cloud))
+    {
+        pcl::console::print_error("Error loading cloud file!\n");
+        return (1);
+    }
+    // --------------------------------
+    // Euclidean Cluster Extract
+    // -----------------------------------
+    pcl::search::KdTree<PointT>::Ptr tree(new pcl::search::KdTree<PointT>);
+    tree->setInputCloud(cloud);
+    std::cout << "border_points has " << cloud->size() << " points" << std::endl;
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<PointT> ec;
+    ec.setClusterTolerance(0.2);
+    ec.setMinClusterSize(200);
+    ec.setMaxClusterSize(100000);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cloud);
+    ec.extract(cluster_indices);
+
+    std::vector<ObjectWindow> object_windows;
+    cv::Mat image;
+    image = cv::imread(image_path, 1);
+
+    int j = 0;
+    // 外循环 循环所有聚类
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
+    {
+
+        ObjectWindow object_window;
+
+        pcl::PointCloud<PointT>::Ptr cloud_cluster(new pcl::PointCloud<PointT>);
+        //内循环 循环一个聚类内部的所有点
+        for (const auto &idx : it->indices)
+        {
+            PointT border_point = (*cloud)[idx];
+            cloud_cluster->push_back(border_point); //*
+            object_window.add_point(border_point);
+        }
+
+        object_window.update();
+        object_window.output();
+        image = object_window.draw(image);
+        object_windows.push_back(object_window);
+
+        // cloud_cluster reset
+        cloud_cluster->width = cloud_cluster->size();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+        // save cluster to pcd
+        std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size() << " data points." << std::endl;
+        std::stringstream ss;
+        ss << "cloud_cluster_" << j << ".pcd";
+        writer.write<PointT>(ss.str(), *cloud_cluster, false); //*
+
+        j++;
+    }
+
+    cv::imshow("show",image);
+    while(true)
+    {
+        if(cv::waitKey(100)==27)
+            break;
+    }
+
+    // std::stringstream ss;
+    // // ss << "pcl_viewer -cam points" << argv[1][6] << ".cam ";
+    // ss << "pcl_viewer ";
+    // for (int i = 0; i < j; i++)
+    // {
+
+    //     ss << " -ps 5  cloud_cluster_" << i << ".pcd";
+    // }
+    // ss << " -ps 1 " << pcd_path;
+    // std::cerr << ss.str() << std::endl;
+
+    // int a = system(ss.str().c_str());
+
+    return 0;
+}
+
+// void show()
+// {
+//     std::vector<pcl::PointIndices> cluster_indices;
+//       int j = 0;
+//   // 外循环 循环所有聚类
+//   for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+//   {
+
+//     ObjectWindow object_window;
+
+//     pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>);
+//     //内循环 循环一个聚类内部的所有点
+//     for (const auto& idx : it->indices)
+//     {
+//         PointT border_point=(*border_points_ptr)[idx];
+//         cloud_cluster->push_back (border_point); //*
+//         object_window.add_point(border_point);
+//     }
+
+//     object_window.update();
+//     object_window.output();
+//     image=object_window.draw(image);
+//     object_windows.push_back(object_window);
+
+//     // cloud_cluster reset
+//     cloud_cluster->width = cloud_cluster->size ();
+//     cloud_cluster->height = 1;
+//     cloud_cluster->is_dense = true;
+//     // save cluster to pcd
+//     std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size () << " data points." << std::endl;
+//     std::stringstream ss;
+//     ss << "cloud_cluster_" << j << ".pcd";
+//     writer.write<PointT> (ss.str (), *cloud_cluster, false); //*
+
+//     j++;
+//   }
+// }
